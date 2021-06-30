12289c1e222ac9a322e4d6bbfba20404e36eebc5
wassil <ouassil.dahimene@gmail.com>
2021-06-30 19:14:26 +0100

    rss feeder added

--- core/models.py
+++ core/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 
+class Publication(models.Model):
+    soouce = models.CharField(max_length=100, blank =True, null=True,verbose_name="Source")
+    author = models.CharField(max_length=100, blank =True, null=True,verbose_name="Author")
+    title = models.CharField(max_length=100, blank =True, null=True,verbose_name="Title")
+    description = models.CharField(max_length=100, blank =True, null=True,verbose_name="Description")
+    image = models.CharField(max_length=100, blank =True, null=True,verbose_name="Image")
+    content=models.TextField(max_length=100, blank =True, null=True,verbose_name="Content")
+    link= models.CharField(max_length=100, blank =True, null=True,verbose_name="Link")
+    date_published=models.DateTimeField(null=True)
 
+    class Meta:
+        verbose_name = "Publication"
+
+    def __str__(self):
+        return str(self.title)
--- core/views.py
+++ core/views.py
@@ -1,13 +1,22 @@
+from core.models import Publication
 from django.shortcuts import render
 import feedparser
+from .models import Publication
+
+
+
    
   
 
 def home(request):
 
     feed=feedparser.parse(r'https://www.algerie360.com/feed/')
-  
+    
+    data={}
+
     for i in  feed.entries:
+        p=Publication(title=i.title)
+        p.save()
         print(i.title)
 
     return render(request,'home.html',feed)
